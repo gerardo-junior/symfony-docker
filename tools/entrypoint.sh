c6ff79a69792bc1cafabe22f284b524698839ae6
Gerardo Junior <me@gerardo-junior.com>
2018-12-19 16:01:52 -0300

    Running chgrp before install

--- tools/entrypoint.sh
+++ tools/entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
+sudo chgrp -Rf www-data ./ 
 
 if [ -f /usr/local/etc/php/conf.d/xdebug.ini ]; then
     sudo sh -c 'sed -i "s/[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}/'$(ip route | awk 'NR==1 {print $3}')'/g" /usr/local/etc/php/conf.d/xdebug.ini'
 fi
 
 if [ -e "$(pwd)/composer.json" ]; then
-    /usr/local/bin/php /usr/local/bin/composer install --no-interaction $(if [[ $DEBUG = "false" ]]; then echo '--no-dev'; fi)
+    /usr/local/bin/php /usr/local/bin/composer install -v --no-interaction $(if [[ $DEBUG = "false" ]]; then echo '--no-dev'; fi)
 fi
 
 if [ ! -z "$1" ]; then
